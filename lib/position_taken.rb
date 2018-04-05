3c36718204ad39917bb6e6f4801d6555bed7d8b3
Sid He <xhe518@gmail.com>
2018-04-05 20:10:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,11 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+input = gets.strip
+index = input_to_index(input)
+
+def input_to_index
+  input.to_i - 1
+end
+
 def position_taken?
 if board[index] == " "
   false
